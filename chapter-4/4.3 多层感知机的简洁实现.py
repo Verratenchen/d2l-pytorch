885707994b350ccbbd82b602f4d9dafd71c29c90
Verratenchen <145882662+Verratenchen@users.noreply.github.com>
2023-10-09 14:48:54 +0800

    Update 4.3 多层感知机的简洁实现.py

--- chapter-4/4.3 多层感知机的简洁实现.py
+++ chapter-4/4.3 多层感知机的简洁实现.py
@@ -4,3 +4,21 @@ from d2l import torch as d2l
 
 
 # 4.3.1
+net = nn.Sequential(nn.Flatten(),
+                    nn.Linear(784, 256),
+                    nn.ReLU(),
+                    nn.Linear(256, 10))
+
+def init_weights(m):
+    if type(m) == nn.Linear:
+        nn.init.normal_(m.weight, std=0.01)
+
+net.apply(init_weights);
+
+batch_size, lr, num_eposhs = 256, 0.1, 10
+loss = nn.CrossEntropyLoss(reduction='none')
+trainer = torch.optim.SGD(net.parameters(), lr=lr)
+
+train_iter, test_iter = d2l.load_data_fashion_mnist(batch_size)
+d2l.train_ch3(net, train_iter, test_iter, loss, num_eposhs, trainer)
+# d2l.plt.show()
